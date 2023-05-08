9c2ede4282cfc1d589c0e790ec032b3e1b7ec48d
Surjeet Singh <s.s8392898116@gmail.com>
2023-05-09 02:54:12 +0530

    React Functional Cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,12 @@ const Home = () => {
 const img2 =
   "https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-HaleBlack-ReLive-Knits-Jogger-FrontRightLogo-Comfortable-Shoes_1024x.jpg?v=1662876260"
 
+  const img3 = "https://rukminim1.flixcart.com/image/832/832/kckud8w0/regionalbooks/e/w/m/the-secret-original-imaftz2dtzdk3tbm.jpeg?q=70"
+
+  const img4 = "https://rukminim1.flixcart.com/image/832/832/ktketu80/mobile/2/y/o/iphone-13-mlpk3hn-a-apple-original-imag6vpyur6hjngg.jpeg?q=70"
+
+  const img5 = "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/q/2/i/m-s-r-b-w-farbot-original-imagjuxy9kn5qyam.jpeg?q=70"
+
 
     const productList = [
         {
@@ -25,11 +31,24 @@ const img2 =
             id: "asdadeadassaerada"
         },
         {
-            name: "Black Shoes",
-            price: 400,
-            imgSrc: img2,
-            id: "asdadeadassaerada"
+            name: "The Secret - Book by Rhonda Byrne",
+            price: 20,
+            imgSrc: img3,
+            id: "asdadeadassaera"
         },
+        {
+            name: "Apple Iphone 12 Pro",
+            price: 699,
+            imgSrc: img4,
+            id: "asdadeadassaerafrr"
+        },
+        {
+            name: "Black Men T-Shirt",
+            price: 8,
+            imgSrc: img5,
+            id: "asdadeadassaerafrr"
+        }
+
     ];
 
 
